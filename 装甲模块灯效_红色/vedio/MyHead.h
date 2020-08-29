6237a79193a2b8516ebf925dd06c852277bb6a5c
Jianing-Wang <55282508+Jianing-Wang@users.noreply.github.com>
2020-08-30 02:20:10 +0800

    Add files via upload

---
+++ 装甲模块灯效_红色/vedio/MyHead.h
@@ -0,0 +1,16 @@
+#ifndef MYHEAD_H
+#define MYHEAD_H
+
+#include "cv.h"
+#include "highgui.h"
+#include "windows.h"
+#include "stdlib.h"
+#include "cv.h"
+#include "highgui.h"
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+
+
+#endif
